8f5bd30b435b8ae3dfa6fcccd584b832367671e0
YoonJY <yoon_jaeyeong#naver.com>
2023-09-11 16:13:36 +0900

    230911

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,20 @@
-$('#main').fullpage({
-    anchors: ['intro', 'p01', 'p02', 'p03', 'p04', 'p05', 'p06', 'tranning', 'profile'],
-    navigation: false,
-    css3: false,
-    scrollOverflow: false,
+$(function () {
+    $('#main').fullpage({
+        anchors: ['intro', 'p01', 'p02', 'p03', 'p04', 'p05', 'p06', 'tranning', 'profile'],
+        navigation: false,
+        css3: false,
+        scrollOverflow: false,
+        parallax: true,
 
-    afterRender: function () {
-        $('#main .section').eq(0).addClass('on');
-        // $('.btn_link li').eq(0).addClass('on').siblings().removeClass('on');
-    },
+        afterRender: function () {
+            $('#main .section').eq(0).addClass('on');
+            // $('.btn_link li').eq(0).addClass('on').siblings().removeClass('on');
+        },
 
-    afterLoad: function (lnk, idx) {
-        $('#main .section').eq(idx - 1).addClass('on').siblings().removeClass('on');
-        $('.btn_link li').eq(idx - 1).addClass('on').siblings().removeClass('on');
-
-    },
+        afterLoad: function (lnk, idx, direction) {
+            $('#main .section').eq(idx - 1).addClass('on').siblings().removeClass('on');
+            $('.btn_link li').eq(idx - 1).addClass('on');
+            $('.btn_link li').eq(idx).removeClass('on');
+        },
+    });
 });
