013b80a3f403b0c0defd5a9501be6d5f18a0767e
YoonJY <yoon_jaeyeong#naver.com>
2023-09-12 14:20:04 +0900

    230912

--- js/custom.js
+++ js/custom.js
@@ -12,8 +12,49 @@ $(function () {
 
         afterLoad: function (lnk, idx) {
             $('#main .section').eq(idx - 1).addClass('on').siblings().removeClass('on');
-            $('.btn_link li').eq(idx - 1).addClass('on');
-            $('.btn_link li').eq(idx).removeClass('on');
+            $('.btn_link li').eq(idx - 1).addClass('on').siblings().removeClass('on');
+            // $('.btn_link li').eq(idx).removeClass('on');
         },
     });
+
+    // mouse pointer
+    const cursor = document.querySelector("#cursor");
+
+        let mouse = { x: -100, y: -100 }
+        let pos = { x: 0, y: 0 }
+        const speed = 0.1
+
+        const updateCoordinates = (e) => {
+            mouse.x = e.clientX
+            mouse.y = e.clientY
+        }
+        window.addEventListener("mousemove", updateCoordinates);
+
+        const updatePosition = () => {
+            pos.x += (mouse.x - pos.x) * speed
+            pos.y += (mouse.y - pos.y) * speed
+            cursor.style.transform =
+                "translate3d(" + pos.x + "px ," + pos.y + "px, 0)"
+        }
+
+        const loop = () => {
+            updatePosition();
+            requestAnimationFrame(loop);
+        }
+
+        requestAnimationFrame(loop);
+
+        const cursorModifiers = document.querySelectorAll("[cursor-class]");
+
+        cursorModifiers.forEach((cursorModifier) => {
+            cursorModifier.addEventListener("mouseenter", function () {
+                let attribute = this.getAttribute("cursor-class");
+                cursor.classList.add(attribute);
+            });
+
+            cursorModifier.addEventListener("mouseleave", function () {
+                let attribute = this.getAttribute("cursor-class");
+                cursor.classList.remove(attribute);
+            });
+        });
 });
